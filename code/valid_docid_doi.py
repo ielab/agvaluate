94c18a21f7f5f90d7d8f16f03a64dd4d3240fb90
Hang Li <cecillll.lee@gmail.com>
2022-02-28 17:47:26 +1000

    add valid docid file and script

---
+++ code/valid_docid_doi.py
@@ -0,0 +1,16 @@
+import json
+import os
+import csv
+
+
+input_json_dir = '../data/all_json/document'
+filenames = [f for f in os.listdir(input_json_dir) if os.path.isfile(os.path.join(input_json_dir, f)) and '.DS_Store' not in f]
+
+with open('valid_docid.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for filename in filenames:
+        if not filename[0].isdigit():
+            file = open(f'{input_json_dir}/{filename}')
+            report_id = json.loads(file.read())['report_id']
+            doi = filename.split('-')[-1].split('.')[0]
+            writer.writerow([report_id, doi])
