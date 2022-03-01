3ce0f9a18cad5324fdd17ad5fe2313c0cc461fb7
Ahmed Mourad <uqamour2@eait.uq.edu.au>
2022-03-01 12:06:02 +1000

    fix the name of the generated dois csv

--- code/extract_articles_dois.py
+++ code/extract_articles_dois.py
@@ -6,7 +6,7 @@ import csv
 input_json_dir = '../data/all_json/document'
 filenames = [f for f in os.listdir(input_json_dir) if os.path.isfile(os.path.join(input_json_dir, f)) and '.DS_Store' not in f]
 
-with open('../data/journal_docid_doi.csv', 'w', newline='') as file:
+with open('../data/article_docid_doi.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for filename in filenames:
         if not filename[0].isdigit():
